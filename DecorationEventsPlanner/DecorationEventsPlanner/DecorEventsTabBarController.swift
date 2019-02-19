eb1e7a213d623badf4808b5980c46313c221e335
Anusha-Meda <iosprojects2018@gmail.com>
2019-02-18 19:57:07 -0500

    Created the Tabcontroller, EventsCalendarVC, PhotosVC with navigation controllers and just added the collection view in storyboard

--- DecorationEventsPlanner/DecorationEventsPlanner/DecorEventsTabBarController.swift
+++ DecorationEventsPlanner/DecorationEventsPlanner/DecorEventsTabBarController.swift
@@ -5,11 +5,11 @@
 //  Created by Anusha on 2/5/19.
 //  Copyright © 2019 Anusha. All rights reserved.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
+class DecorEventsTabBarController: UITabBarController {
 
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
